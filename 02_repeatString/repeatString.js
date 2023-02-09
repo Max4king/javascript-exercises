4f6654c41a4f8021729f9130d77621b5f851e369
Max4king <ryan3311@hotmail.com>
2023-02-09 16:32:24 +0100

    first two exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let str = "";
+    for (i=0; i<num;i++) {
+        str = str + word;
+    }
+    if (num < 0) return "ERROR";
+    return str;
 };
 
 // Do not edit below this line

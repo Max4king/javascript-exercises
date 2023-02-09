036ca0561e862240cd65f164de8649adf3abc8d8
Max4king <ryan3311@hotmail.com>
2023-02-09 22:22:30 +0100

    ex 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...val) {
+    for (let i=0; i < val.length; i++) {
+        let index = arr.indexOf(val[i]);
+        console.log(index);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line

2e31d45a75f10307a3c55494dc4f617d1aa2f61f
Max4king <ryan3311@hotmail.com>
2023-02-09 22:34:48 +0100

    ex 6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total = 0;
+    if ( !Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    for (let i=start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
+
+let ans = sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+console.log(ans);
+
 
 // Do not edit below this line
 module.exports = sumAll;

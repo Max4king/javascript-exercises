12481a8f895788eb34c327fdf192ad6193a1a35f
Max4king <ryan3311@hotmail.com>
2023-02-09 16:44:41 +0100

    test 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let tmp = "";
+    let new_string = (string.split("")).reverse();
+    return new_string.join("");
 };
 
 // Do not edit below this line

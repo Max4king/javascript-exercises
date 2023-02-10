a305775f5ef9a93639dd32cb858b366cb278007c
Max King <ryan3311@hotmail.com>
2023-02-10 15:57:15 +0100

    real ex6 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  result = (fah -32) * 5 / 9;
+  result = Math.floor(result * 100) / 10;
+  result = Math.round(result) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  result = (cel * 9 / 5) + 32
+  result = Math.floor(result * 100) / 10;
+  result = Math.round(result) / 10;
+  return result;
 };
 
 // Do not edit below this line

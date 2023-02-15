fde7b24944f3c9ad19977060b66df86451b2c93b
Max King <ryan3311@hotmail.com>
2023-02-15 12:29:00 +0100

    ex 9 doen

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let new_str = str.replace(/[^a-zA-Z]+/g,"")
+    let arr = new_str.toLowerCase() .split("");
+    let arr_len = arr.length - 1;
+    for (let i=0; i < arr_len/2;i++) {
+        if (arr[i] !== arr[arr_len-i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+let ans = palindromes('Animal loots foliated detail of stool lamina.');
+
+console.log(ans)
 
 // Do not edit below this line
 module.exports = palindromes;

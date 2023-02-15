34d74876bfc208cf9b54972e69d95e5accffaadc
Max King <ryan3311@hotmail.com>
2023-02-15 12:38:25 +0100

    ex 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let a = 1;
+    let b = 1;
+    if (index < 0) {
+        return "OOPS"
+    }
 
+    for (i=1;i < index-1;i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
